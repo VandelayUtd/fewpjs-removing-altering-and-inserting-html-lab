7a2fe00e78eea34bf20315e6c8ff95591b76dbda
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-20 20:13:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+// let element = document.createElement('div')
+
+const main = document.querySelector("main")
+
+main.remove()
+
+let newHeader = document.createElement("h1")
+
+
+
+document.body.appendChild(newHeader)
+
+newHeader.id = "victory"
+newHeader.class = "victory"
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
